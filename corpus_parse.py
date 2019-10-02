0c9b121c55a2e1d91f99549fa228765bead243f9
poteminr <poteminr@gmail.com>
2019-10-02 22:16:53 +0700

    add corpus preproc

---
+++ corpus_parse.py
@@ -0,0 +1,30 @@
+import vk
+from time import sleep
+from tqdm import tqdm_notebook
+
+# parser init params
+TOKEN = "KEY"
+session = vk.Session(access_token=TOKEN)
+vk_api = vk.API(session)
+
+# target init params
+GROUP_ID = 71729358
+
+
+
+def get_data(group_id=GROUP_ID, offset_coeff=10):
+
+    group_id = group_id * -1
+    
+    
+    first_init = vk_api.wall.get(owner_id=group_id, v=5.84)
+    content = first_init["items"]
+    count = first_init["count"] // offset_coeff
+
+    for i in tqdm_notebook(range(1, count+1)):
+        content += vk_api.wall.get(owner_id=group_id, v=5.84, offset=i*offset_coeff, filter="other")["items"]
+
+
+    print("Total posts: {} | Offset coefficient: {}".format(len(content), offset_coeff))
+
+    return content
---
+++ data_validate.py
@@ -0,0 +1,65 @@
+import re, string, unicodedata
+from tqdm import tqdm_notebook
+
+ban_list = ["vk.com", "Бот", "bot", "club"]
+def check_valid(data_dct: dict):
+    
+    if "attachments" in data_dct.keys():
+        return False
+    
+    text = data_dct['text']
+    
+    for cond in ban_list:
+            if len(re.findall(cond, text)) > 0:
+                return False
+
+    return True
+
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+def remove_punctuation(tokens):
+    new_words = []
+    for word in tokens:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+u"\U0001F600-\U0001F64F"  
+u"\U0001F300-\U0001F5FF"  
+u"\U0001F680-\U0001F6FF"  
+u"\U0001F1E0-\U0001F1FF"  
+u"\U00002702-\U000027B0"
+u"\U000024C2-\U0001F251"
+"]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+
+def data_preproc(data: list, remove_punct=True, remove_smiles=True, tokenize_punct=False):
+    assert remove_punct != tokenize_punct, "Use one of this parameters"
+
+    full_data = []
+    for sent in tqdm_notebook(data):
+        if check_valid(sent):
+            text = sent['text']
+
+            text = text.replace("\n", " ").replace("  ", " ")
+
+            if remove_smiles:
+                text = remove_emoji(text)
+
+            if tokenize_punct:
+                text_tokens = re.findall(r"[\w']+|[.,!?;]", text)
+            else:
+                text_tokens = text.split(" ")
+
+            if remove_punct:
+                text_tokens = remove_punctuation(text_tokens)
+
+            full_data.append(text_tokens)
+        
+    print("Samples: {} | Removed samples: {}".format(len(full_data), len(data) - len(full_data)))
+    return full_data
